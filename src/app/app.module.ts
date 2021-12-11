4c5aec3a042d542fdbd5036b01e1a900aab88443
sim1-dev <simonetenisci10@gmail.com>
2021-12-11 17:17:38 +0100

    public release

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { UploadformComponent } from './uploadform/uploadform.component';
+import { NavFooterComponent } from './nav-footer/nav-footer.component';
+import { NavHeaderComponent } from './nav-header/nav-header.component';
 
 @NgModule({
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   declarations: [
     AppComponent,
-    UploadformComponent
+    UploadformComponent,
+    NavFooterComponent,
+    NavHeaderComponent
   ],
   imports: [
     BrowserModule,
